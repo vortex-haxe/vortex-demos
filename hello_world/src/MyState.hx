438fb0de53ccbcd7ce7db56f2cc0cc77a3c3b35f
swordcube <49110074+swordcube@users.noreply.github.com>
2024-05-18 07:44:06 -0500

    fully recreate the old hello world demo

--- hello_world/src/MyState.hx
+++ hello_world/src/MyState.hx
@@ -1,19 +1,24 @@
 package;
 
 import vortex.State;
+import vortex.GlobalCtx;
 import vortex.display.Sprite;
 
 class MyState extends State {
     var spr:Sprite;
 
     override function ready() {
-        super.ready();
-        spr = new Sprite(-30, -30).loadGraphic("assets/vortex_logo.png");
+        GlobalCtx.camera.bgColor = 0xFF292533;
+
+        spr = new Sprite(100, 100).loadGraphic("assets/vortex_logo.png");
+        spr.screenCenter();
         add(spr);
+
+        super.ready();
     }
 
     override function update(delta:Float) {
-        spr.position.x += delta * 60;
+        spr.angle += delta * 25;
         super.update(delta);
     }
 }
